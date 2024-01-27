44b61f70885db949dee8ab8f032b84f744a4be5a
Summaa <manbat7776@yahoo.com>
2024-01-26 22:22:16 -0500

    Added function to draw lines from a center point of one cell to another depending on position. Cell sides must be touching or there will only be half lines.

--- cell.py
+++ cell.py
@@ -14,6 +14,8 @@ class Cell:
         self.win = win
     
     def draw(self, x1, x2, y1, y2):
+        if self.win is None:
+            return
         self.x1 = x1
         self.x2 = x2
         self.y1 = y1
@@ -30,3 +32,40 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        if self.win is None:
+            return
+        x_mid = (self.x1 + self.x2) / 2
+        y_mid = (self.y1 + self.y2) / 2
+        to_x_mid = (to_cell.x1 + to_cell.x2) / 2
+        to_y_mid = (to_cell.y1 + to_cell.y2) / 2
+
+        fill_color = "red"
+        if undo:
+            fill_color = "grey"
+
+        #left
+        if self.x1 > to_cell.x1:
+            line = Line(Point(self.x1, y_mid), Point(x_mid, y_mid))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_cell.x2, to_y_mid), Point(to_x_mid, to_y_mid))
+            self.win.draw_line(line, fill_color)
+        #right
+        if self.x1 < to_cell.x1:
+            line = Line(Point(self.x2, y_mid), Point(x_mid, y_mid))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_y_mid), Point(to_cell.x1, to_y_mid))
+            self.win.draw_line(line, fill_color)
+        #up
+        if self.y1 > to_cell.y1:
+            line = Line(Point(x_mid, self.y1), Point(x_mid, y_mid))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_cell.y2), Point(to_x_mid, to_y_mid))
+            self.win.draw_line(line, fill_color)
+        #down
+        if self.y1 < to_cell.y1:
+            line = Line(Point(x_mid, self.y2), Point(x_mid, y_mid))
+            self.win.draw_line(line, fill_color)
+            line = Line(Point(to_x_mid, to_cell.y1), Point(to_x_mid, to_y_mid))
+            self.win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -3,18 +3,25 @@ from cell import *
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win).has_left_wall = False
+    cell1 = Cell(win)
     cell1.has_left_wall = False
+    cell1.draw(100, 150, 100, 150)
+
     cell2 = Cell(win)
     cell2.has_top_wall = False
+    cell2.draw(100, 150, 151, 201)
+
     cell3 = Cell(win)
     cell3.has_right_wall = False
+    cell3.draw(151, 201, 100, 150)
+
     cell4 = Cell(win)
     cell4.has_bottom_wall = False
-    cell1.draw(100, 150, 100, 150)
-    cell2.draw(100, 150, 151, 201)
-    cell3.draw(151, 201, 100, 150)
     cell4.draw(151, 201, 151, 201)
+
+    cell1.draw_move(cell2)
+    cell2.draw_move(cell4, True)
+
     win.wait_for_close()
 
 main()
