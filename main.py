b2d3b5630c317177dfe412e66c7be357a4f3298b
Summaa <manbat7776@yahoo.com>
2024-01-27 01:42:52 -0500

    Added basic maze generator

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 from graphics import *
 from cell import *
+from maze import *
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell1.has_left_wall = False
-    cell1.draw(100, 150, 100, 150)
-
-    cell2 = Cell(win)
-    cell2.has_top_wall = False
-    cell2.draw(100, 150, 151, 201)
-
-    cell3 = Cell(win)
-    cell3.has_right_wall = False
-    cell3.draw(151, 201, 100, 150)
-
-    cell4 = Cell(win)
-    cell4.has_bottom_wall = False
-    cell4.draw(151, 201, 151, 201)
-
-    cell1.draw_move(cell2)
-    cell2.draw_move(cell4, True)
+    maze = Maze(20,20, 15, 15, 20, 20, win)
+    maze.create_cells()
 
     win.wait_for_close()
 
---
+++ maze.py
@@ -0,0 +1,46 @@
+from cell import *
+import time
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_columns,
+            cell_size_x,
+            cell_size_y,
+            win,
+            ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_columns = num_columns
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.cells = []
+        self.create_cells()
+
+    def create_cells(self):
+        self.cells = [[Cell(self.win) for row in range(self.num_rows)] for column in range(self.num_columns)]
+        for column_index, column in enumerate(self.cells):
+            for row_index, cell in enumerate(column):
+                self.draw_cell(column_index, row_index)
+
+    def draw_cell(self, i, j,):
+        if self.win is None:
+            return
+        cell = self.cells[i][j]
+        x1 = self.x1 + j * self.cell_size_x
+        y1 = self.y1 + i * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+        cell.draw(x1, x2, y1, y2)
+        self.animate()
+
+    def animate(self):
+        if self.win is None:
+            return
+        self.win.redraw()
+        time.sleep(.05)
