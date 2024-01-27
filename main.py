e9b8bbacb5f39a660866c9f5e53fbad53db8926e
Summaa <manbat7776@yahoo.com>
2024-01-26 20:31:33 -0500

    Added line creation and point creation. Added .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from graphics import Window
+from graphics import *
 
 def main():
     win = Window(800, 600)
+    p1 = Point(100, 100)
+    p2 = Point(500, 100)
+    p3 = Point(500, 500)
+    p4 = Point(100, 500)
+    win.draw_line(Line(p1, p2))
+    win.draw_line(Line(p2, p3))
+    win.draw_line(Line(p3, p4))
+    win.draw_line(Line(p4, p1))
     win.wait_for_close()
 
 main()
